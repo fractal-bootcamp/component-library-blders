1f53def55ca7f877ecf060917edd9e1364e37c51
Lui <6627294+yablochko8@users.noreply.github.com>
2024-07-22 11:19:04 -0400

    basic styling

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -11,21 +11,31 @@ type TextInputProps = {
 }
 
 /**
- * Behavior/props:
-
-- Validation states (error, success)
-- Toggle hidden password option
-- Disabled state
-- Prefix and suffix icons
-- Popover info icon
-- Placeholder text
-- Value and onChange handler
-
-Default Animation:
-
-- Focus animation with border color change and slight shadow.
-- Shake animation on validation error.
- * @returns 
+ * Features and Props:
+ * 
+ * - Supports validation states such as error and success.
+ * - Allows toggling of hidden password option.
+ * - Ability to set the input field as disabled.
+ * - Option to include prefix and suffix icons.
+ * - Supports popover info icon.
+ * - Placeholder text can be customized.
+ * - Provides value and onChange handler for input control.
+ * 
+ * Default Animations:
+ * 
+ * - Focus animation includes border color change and slight shadow effect.
+ * - Shake animation triggered on validation error.
+ * 
+ * @param {string} value - The current value of the input field.
+ * @param {Function} onChange - The function to handle changes in the input field.
+ * @param {string} placeholderText - The text to display as a placeholder in the input field.
+ * @param {boolean} isPassword - Indicates if the input field should be of type password.
+ * @param {boolean} isDisabled - Indicates if the input field should be disabled.
+ * @param {string} prefixIcon - The icon to display as a prefix in the input field.
+ * @param {string} suffixIcon - The icon to display as a suffix in the input field.
+ * @param {"null" | "error" | "success"} validationState - The validation state of the input field.
+ * 
+ * @returns {JSX.Element} 
  */
 export const TextInput = ({ value, onChange, placeholderText, isPassword, isDisabled, prefixIcon, suffixIcon, validationState }: TextInputProps) => {
     return (
