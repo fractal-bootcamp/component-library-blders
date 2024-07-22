90b8f8b64231d6ba51d7085877ef93f96a6d4832
dxren <dorothy.x.ren@gmail.com>
2024-07-22 11:51:01 -0400

    basic first button functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import './index.css'
 import { FirstButton } from './components/Button'
+import { useState } from 'react'
 
 function App() {
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  function handleClick() {
+    setIsDisabled(true);
+    setTimeout(() => { setIsDisabled(false) }, 1000);
+    console.log('clicked');
+  }
+
   return (
     <>
       <div className='bg-red-100'>
         <div className="text-3xl text-red font-bold underline">
           Hello world!
         </div>
-        <FirstButton />
+        <FirstButton disabled={isDisabled} text={'Click me'} onClick={handleClick} />
+        <button onClick={() => { setIsDisabled(false) }}>Cancel</button>
       </div>
     </>
   )
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,3 +1,32 @@
-export const FirstButton = () => {
-    return <div />
+interface ButtonProps {
+    // variant?: 'primary' | 'secondary' | 'tertiary';
+    // size?: 'small' | 'medium' | 'large';
+    disabled?: boolean;
+    // loading?: boolean;
+    onClick: () => void;
+    text: string;
+    // tooltip?: string;
 }
+
+export const FirstButton = ({ disabled = false, onClick, text }: ButtonProps) => {
+
+    const baseClasses = 'flex justify-center px-4 py-2 font-bold text-white bg-blue-500 rounded-md hover:bg-blue-700 focus:outline-none'
+
+    const disabledClasses = 'cursor-not-allowed opacity-50'
+
+    const classes = `
+    ${baseClasses}
+    ${disabled && disabledClasses}`;
+
+    return (
+        <div>
+            <button
+                className={classes}
+                disabled={disabled}
+                onClick={onClick}
+            >{text}</button>
+        </div>
+    )
+}
+
+export default FirstButton
