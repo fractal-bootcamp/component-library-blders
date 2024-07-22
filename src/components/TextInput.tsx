b802a526759d0f954dce9a03fa5af1c62e27cfb3
Lui <6627294+yablochko8@users.noreply.github.com>
2024-07-22 11:20:15 -0400

    icons

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -38,14 +38,30 @@ type TextInputProps = {
  * @returns {JSX.Element} 
  */
 export const TextInput = ({ value, onChange, placeholderText, isPassword, isDisabled, prefixIcon, suffixIcon, validationState }: TextInputProps) => {
+
+    const sharedStyle = "rounded border p-2 m-2";
+    const regStyle = sharedStyle;
+    const successStyle = sharedStyle + " border-green-500";
+    const errorStyle = sharedStyle + " border-red-500";
+
     return (
-        <input
-            type={isPassword ? "password" : "text"}
-            value={value}
-            onChange={(e) => onChange(e.target.value)}
-            placeholder={placeholderText}
-            disabled={isDisabled}
-            className={validationState === "error" ? "error" : validationState === "success" ? "success" : ""}
-        />
+        <>
+            <div>
+                {prefixIcon && <span className="absolute left-0 inset-y-0 flex items-center pl-2">
+                    <img src={prefixIcon} alt="prefix icon" className="h-4 w-4" />
+                </span>}
+                <input
+                    type={isPassword ? "password" : "text"}
+                    value={value}
+                    onChange={(e) => onChange(e.target.value)}
+                    placeholder={placeholderText}
+                    disabled={isDisabled}
+                    className={validationState === "error" ? errorStyle : validationState === "success" ? successStyle : regStyle}
+                />
+            </div>
+            {suffixIcon && <span className="absolute right-0 inset-y-0 flex items-center pr-2">
+                <img src={suffixIcon} alt="suffix icon" className="h-4 w-4" />
+            </span>}
+        </>
     );
 }
