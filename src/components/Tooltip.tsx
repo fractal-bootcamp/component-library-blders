acb1bfbb462a688a08da12d5cd22d46ec523942e
Brian Smiley <briantsmiley42@gmail.com>
2024-07-22 15:29:40 -0400

    updates styling to space off the parent element

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -3,7 +3,7 @@ import "../index.css";
 interface TooltipProps {
   children: React.ReactNode; //the page elements being wrapped which should display tooltip when hovered
   tipContent: React.ReactNode; //the text/other content to render in the tooltip, passed in as a text/JSX/whatever prop
-  position: "top" | "bottom" | "left" | "right"; //the position of the tooltip relative to the wrapped element
+  position: "top" | "bottom" | "left" | "right" | "cursor"; //the position of the tooltip relative to the wrapped element
 }
 export default function Tooltip({
   children,
@@ -16,13 +16,13 @@ export default function Tooltip({
   const positionClass = useMemo(() => {
     switch (position) {
       case "top":
-        return "absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-full";
+        return "absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-[calc(100%+0.5rem)] ";
       case "bottom":
-        return "absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-full";
+        return "absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-[calc(100%+0.5rem)]";
       case "left":
-        return "absolute left-0 top-1/2 transform -translate-y-1/2 -translate-x-full";
+        return "absolute left-0 top-1/2 transform -translate-y-1/2 -translate-x-[calc(100%+0.5rem)]";
       case "right":
-        return "absolute right-0 top-1/2 transform -translate-y-1/2 translate-x-full";
+        return "absolute right-0 top-1/2 transform -translate-y-1/2 translate-x-[calc(100%+0.5rem)]";
       default:
         return "";
     }
@@ -32,10 +32,12 @@ export default function Tooltip({
       className="relative inline-block bg-slate-200"
       onMouseEnter={() => setShow(true)}
       onMouseLeave={() => setShow(false)}
+      onFocus={() => setShow(true)}
+      onBlur={() => setShow(false)}
     >
       {/* The tooltip */}
       <div
-        className={`${positionClass} ${show ? "" : "hidden"} flex justify-center items-center text-center w-max  bg-gray-800 text-white p-2 rounded-md shadow-lg`}
+        className={`${positionClass} ${show ? "opacity-100" : "opacity-0"} transition-opacity duration-300 ease-in-out flex justify-center items-center text-center w-max  bg-gray-800 text-white p-2 rounded-md shadow-lg`}
       >
         {tipContent}
       </div>
