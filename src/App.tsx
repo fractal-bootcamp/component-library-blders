93a0efa2207958ebd64fc705c8d55cc0f54e7a01
dxren <dorothy.x.ren@gmail.com>
2024-07-23 10:48:15 -0400

    update button branch based on feedback, apply stashed changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import './index.css'
 import { FirstButton } from './components/Button'
 
+
 function App() {
   return (
     <>
-      <div className='bg-red-100'>
-        <div className="text-3xl text-red font-bold underline">
-          Hello world!
-        </div>
-        <FirstButton />
+      <div className='bg-red-100 flex flex-col items-center gap-4 m-5 p-5 rounded-md'>
+        <FirstButton
+          variant='primary'
+          size='medium'
+          textContent='Click me'
+          icon={'rocket'}
+          iconPosition='left'
+        />
       </div>
     </>
   )
