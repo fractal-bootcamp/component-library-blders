92c7638336ed00a917d581441088f7284b682b06
Brian Smiley <briantsmiley42@gmail.com>
2024-07-23 15:28:01 -0400

    repositions selection display in storybook

--- src/stories/DropdownParent.tsx
+++ src/stories/DropdownParent.tsx
@@ -5,10 +5,9 @@ export interface DropdownParentProps {
   multiple?: boolean;
 }
 const DropdownParent = ({ options, multiple = false }: DropdownParentProps) => {
-  const [selected, setSelected] = useState([""]);
+  const [selected, setSelected] = useState<string[]>([]);
   return (
-    <div className="flex flex-col gap-2 items-center justify-center">
-      Selections: {selected.join(", ") || "-"}
+    <div className="flex flex-col gap-2 items-start justify-start">
       <div className="w-fit">
         <Dropdown
           options={options}
@@ -17,7 +16,14 @@ const DropdownParent = ({ options, multiple = false }: DropdownParentProps) => {
           setSelected={setSelected}
         />
       </div>
-      <div>Stuff beneath the dropdown</div>
+      <div className="relative">
+        <div className="absolute -z-10 top-0 left-0 flex flex-col text-xs">
+          Selections:
+          {selected.map(item => (
+            <span>{item}</span>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
