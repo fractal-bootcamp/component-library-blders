0f853a598e350fe4198d8a48b10369e202e373c7
Brian Smiley <briantsmiley42@gmail.com>
2024-07-23 16:10:24 -0400

    restyles and adds placeholder to search

--- src/components/DropdownSearch.tsx
+++ src/components/DropdownSearch.tsx
@@ -9,9 +9,11 @@ const DropdownSearch = ({
   setExpanded
 }: DropdownSearchProps) => {
   return (
-    <div className={`bg-slate-50 px-4 py-2 w-full cursor-pointer`}>
+    <div className={`bg-slate-50 w-full cursor-pointer`}>
       <input
         type="text"
+        className="bg-slate-200 px-4 py-2 "
+        placeholder="Search..."
         value={searchTerm}
         onFocus={() => setExpanded(true)}
         onChange={e => setSearchTerm(e.target.value)}
