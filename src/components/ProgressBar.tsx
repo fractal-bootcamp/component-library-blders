3a8e52d9ff37e0ebcdbcd69d9ad840eead3355e6
dxren <dorothy.x.ren@gmail.com>
2024-07-23 14:05:36 -0400

    added linear/circular with daisy, will go back and do svg version. and color customization based on progress

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,36 +1,66 @@
 
 interface ProgressBarProps {
-    color?: string;
-    // shape?: 'linear' | 'circular';
-    percentage: number;
-    progress?: 'first' | 'second' | 'third';
+    shape: 'linear' | 'circular';
+    value: number;
+    max: number;
     // striped?: boolean;
     // animated?: boolean;
 }
 
-export const ProgressBar = ({ color, percentage = 0, progress = 'first' }: ProgressBarProps) => {
+export const ProgressBar = ({ shape, value, max }: ProgressBarProps) => {
     const baseClass = 'h-6 w-full bg-gray-200 rounded-md border border-gray-400'
 
-    const progressClasses = {
+    const progressClassesLinear = {
         first: 'bg-red-500',
-        second: 'bg-yellow-500',
-        third: 'bg-green-500',
+        second: 'bg-orange-500',
+        third: 'bg-yellow-500',
+        fourth: 'bg-green-500',
+    };
+
+    const progressClassesCircular = {
+        first: 'text-red-500',
+        second: 'text-orange-500',
+        third: 'text-yellow-500',
+        fourth: 'text-green-500',
+    };
+
+    const percentage = Math.round((value / max) * 100);
+
+    const getProgressBarColorLinear = () => {
+        if (percentage < 25) return progressClassesLinear.first;
+        if (percentage < 50) return progressClassesLinear.second;
+        if (percentage < 75) return progressClassesLinear.third;
+        return progressClassesLinear.fourth;
     }
 
-    const classes = `
-        ${baseClass}
-        ${progressClasses[progress]}`
-
+    const getProgressBarColorCircular = () => {
+        if (percentage < 25) return progressClassesCircular.first;
+        if (percentage < 50) return progressClassesCircular.second;
+        if (percentage < 75) return progressClassesCircular.third;
+        return progressClassesCircular.fourth;
+    }
 
     return (
-        <div className={classes}>
-            <div style={{ width: `${percentage}% `, backgroundColor: color }}
-                className='h-full rounded-md'>
-            </div>
-            <div className="text-black text-sm font-bold px-3 p-1">
-                {percentage}%
-            </div>
-        </div>
+        <>
+            {shape === 'circular' &&
+                <div className={`radial-progress bg-gray-200 ${getProgressBarColorCircular()}`} style={{ "--value": `${percentage}` } as React.CSSProperties} role="progressbar">
+                    <div className="text-black text-sm font-bold px-3 p-1">
+                        {percentage}%
+                    </div>
+                </div>
+            }
+            {shape === 'linear' &&
+                <div className={baseClass}>
+                    <div style={{ width: `${percentage}% ` }}
+                        className={` h-full rounded-md transition-all duration-399 ease-in-out ${getProgressBarColorLinear()}`}>
+                    </div>
+                    <div className="text-black text-sm font-bold px-3 p-1">
+                        {percentage}%
+                    </div>
+                </div>
+            }
+
+        </>
     );
 }
 
